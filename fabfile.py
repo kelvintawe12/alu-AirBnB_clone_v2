1a57c7e077ca77b1c1b464b91b663db901ca4f57
TAWE KELVIN <169045295+kelvintawe12@users.noreply.github.com>
2025-02-07 19:24:51 +0200

    done

--- fabfile.py
+++ fabfile.py
@@ -4,16 +4,14 @@ from datetime import datetime
 
 @task
 def do_pack(c):
-    """Generates a .tgz archive from the contents of web_static"""
-    time_stamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive_path = f"versions/web_static_{time_stamp}.tgz"
-
+    """Generates a .tgz archive from the web_static folder"""
+    
     if not os.path.exists("versions"):
         os.makedirs("versions")
-
-    result = c.run(f"tar -cvzf {archive_path} web_static", hide=False)
-
-    if result.ok:
-        return archive_path
-    else:
-        return None
+    
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_path = f"versions/web_static_{timestamp}.tgz"
+    
+    c.run(f"tar -cvzf {archive_path} web_static")
+    
+    return archive_path if os.path.exists(archive_path) else None
